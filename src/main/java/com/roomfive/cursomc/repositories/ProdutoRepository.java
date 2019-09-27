6750b1ebc6da640db05bde249d8d71167e2e625d
Samuel Bento <samuel94bento@gmail.com>
2019-09-27 13:21:55 +0100

    Produto e associação muitos para muitos

---
+++ src/main/java/com/roomfive/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.roomfive.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.roomfive.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
