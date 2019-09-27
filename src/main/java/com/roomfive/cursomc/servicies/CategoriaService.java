63d542d29b4ed44cc2912ee342a64b1851cbf9d1
Samuel Bento <samuel94bento@gmail.com>
2019-09-27 11:57:53 +0100

    Service e repository Categoria

---
+++ src/main/java/com/roomfive/cursomc/servicies/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.roomfive.cursomc.servicies;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.roomfive.cursomc.domain.Categoria;
+import com.roomfive.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> cat = repo.findById(id);
+		return cat.orElse(null);
+		
+	}
+
+}
